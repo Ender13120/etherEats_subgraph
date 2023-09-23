743e318a903dea490f49e64be36ec5409efb640e
Ender <117198798+Ender13120@users.noreply.github.com>
2023-09-23 22:33:32 +0200

    add User Entity for easier Querying

--- src/ether-eats.ts
+++ src/ether-eats.ts
@@ -7,8 +7,11 @@ import {
   OrderCompleted as OrderCompletedEvent,
   OrderCreated as OrderCreatedEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
-  TokensSlashed as TokensSlashedEvent
-} from "../generated/EtherEats/EtherEats"
+  TokensSlashed as TokensSlashedEvent,
+} from "../generated/EtherEats/EtherEats";
+
+import { BigInt } from "@graphprotocol/graph-ts";
+
 import {
   CustomerReputationIncreased,
   DelivererReputationDecreased,
@@ -18,23 +21,28 @@ import {
   OrderCompleted,
   OrderCreated,
   OwnershipTransferred,
-  TokensSlashed
-} from "../generated/schema"
+  TokensSlashed,
+  User,
+} from "../generated/schema";
 
 export function handleCustomerReputationIncreased(
   event: CustomerReputationIncreasedEvent
 ): void {
   let entity = new CustomerReputationIncreased(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.customer = event.params.customer
-  entity.amount = event.params.amount
+  );
+  entity.customer = event.params.customer;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  let user = getOrCreateUser(event.params.customer.toHexString());
+  user.customerReputation = user.customerReputation.plus(event.params.amount);
+  user.save();
+
+  entity.save();
 }
 
 export function handleDelivererReputationDecreased(
@@ -42,15 +50,31 @@ export function handleDelivererReputationDecreased(
 ): void {
   let entity = new DelivererReputationDecreased(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.deliverer = event.params.deliverer
-  entity.amount = event.params.amount
+  );
+  entity.deliverer = event.params.deliverer;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
+
+  let user = getOrCreateUser(event.params.deliverer.toHexString());
+  user.reputation = user.reputation.minus(event.params.amount);
+  user.save();
+
+  entity.save();
+}
+
+function getOrCreateUser(userId: string): User {
+  let user = User.load(userId);
+  if (user == null) {
+    user = new User(userId);
+    user.reputation = BigInt.fromI32(0);
+    user.customerReputation = BigInt.fromI32(0);
+  }
+  return user as User;
 }
 
 export function handleDelivererReputationIncreased(
@@ -58,71 +82,75 @@ export function handleDelivererReputationIncreased(
 ): void {
   let entity = new DelivererReputationIncreased(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.deliverer = event.params.deliverer
-  entity.amount = event.params.amount
+  );
+  entity.deliverer = event.params.deliverer;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  let user = getOrCreateUser(event.params.deliverer.toHexString());
+  user.reputation = user.reputation.plus(event.params.amount);
+  user.save();
+
+  entity.save();
 }
 
 export function handleOrderAccepted(event: OrderAcceptedEvent): void {
   let entity = new OrderAccepted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
-  entity.deliverer = event.params.deliverer
+  );
+  entity.orderId = event.params.orderId;
+  entity.deliverer = event.params.deliverer;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOrderCancelled(event: OrderCancelledEvent): void {
   let entity = new OrderCancelled(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
-  entity.by = event.params.by
+  );
+  entity.orderId = event.params.orderId;
+  entity.by = event.params.by;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOrderCompleted(event: OrderCompletedEvent): void {
   let entity = new OrderCompleted(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
+  );
+  entity.orderId = event.params.orderId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOrderCreated(event: OrderCreatedEvent): void {
   let entity = new OrderCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.orderId = event.params.orderId
-  entity.customer = event.params.customer
-  entity.encryptedParams = event.params.encryptedParams
+  );
+  entity.orderId = event.params.orderId;
+  entity.customer = event.params.customer;
+  entity.encryptedParams = event.params.encryptedParams;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -130,27 +158,27 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
 
 export function handleTokensSlashed(event: TokensSlashedEvent): void {
   let entity = new TokensSlashed(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.deliverer = event.params.deliverer
-  entity.amount = event.params.amount
+  );
+  entity.deliverer = event.params.deliverer;
+  entity.amount = event.params.amount;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.save()
+  entity.save();
 }
